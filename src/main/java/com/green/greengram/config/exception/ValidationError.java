002bc839c6e10b0c19bc3bd07b8cb3cd68257b78
nususu1004 <nususu1004@gmail.com>
2025-08-06 16:58:58 +0900

    greengram validation 정규식으로 유효성 작업 중 2

--- src/main/java/com/green/greengram/config/exception/ValidationError.java
+++ src/main/java/com/green/greengram/config/exception/ValidationError.java
@@ -1,24 +1,16 @@
 package com.green.greengram.config.exception;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 import org.springframework.validation.FieldError;
 
 @Getter
-@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
 public class ValidationError {
     private String field;
     private String message;
 
     public static ValidationError of(final FieldError fieldError) {
-        return ValidationError.builder()
-                .field(fieldError.getField())
-                .message(fieldError.getDefaultMessage())
-                .build();
-    }
-
-    @Override
-    public String toString() {
-        return String.format("field: %s, message: %s", field, message);
+        return new ValidationError(fieldError.getField(), fieldError.getDefaultMessage());
     }
 }
